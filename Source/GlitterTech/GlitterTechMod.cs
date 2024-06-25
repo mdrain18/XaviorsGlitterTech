1178ef2719946d37a99cb2cb23426f7745763eef
Michael Rainey <mdrain18@gmail.com>
2024-06-25 14:31:00 -0400

    Reorganization and bug fixes
    Reorganized all the files based on 1.5's file organization and XML structure
    Fixed the OC Exo verb error

---
+++ Source/GlitterTech/GlitterTechMod.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Verse;
+
+namespace GlitterTech
+{
+    // Main class for the GlitterTech mod
+    class GlitterTechMod : Mod
+    {
+        public static GlitterTechSettings settings;
+
+        public GlitterTechMod(ModContentPack content) : base(content)
+        {
+            // Initializing Harmony with a unique ID
+            var harmony = new Harmony("com.xavior.glittertech");
+            harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+
+            // Load settings
+            settings = GetSettings<GlitterTechSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard ls = new Listing_Standard();
+            ls.Begin(inRect);
+
+            // Creating a slider in the settings window
+            ls.Label($"Resource Stat Multiplier: {settings.statMultiplier:P0} (default is 100%)");
+            settings.statMultiplier = ls.Slider(settings.statMultiplier, 0.01f, 2.00f);
+
+            ls.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "GlitterTech";
+        }
+    }
+}
---
+++ Source/GlitterTech/GlitterTechSettings.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+namespace GlitterTech
+{
+    // Settings class for storing mod-specific settings
+    public class GlitterTechSettings : ModSettings
+    {
+        public float statMultiplier = 1.00f; // 100% default
+
+        // Method to handle saving/loading of settings
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref statMultiplier, "statMultiplier", 1.00f);
+            base.ExposeData();
+        }
+    }
+}

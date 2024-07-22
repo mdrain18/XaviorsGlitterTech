250ac43fe10649b301926ed4f3ca81f5d17a4079
Michael Rainey <mdrain18@gmail.com>
2024-07-22 16:52:33 -0400

    Bionics Update
    Made some changes to the glitter tech bionics.

--- Source/GlitterTech/Patches/GlitterTechPatcher.cs
+++ Source/GlitterTech/Patches/GlitterTechPatcher.cs
@@ -1,5 +1,8 @@
-﻿using HarmonyLib;
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
 using Verse;
+using RimWorld;
 
 namespace GlitterTech
 {
@@ -10,6 +13,28 @@ namespace GlitterTech
         {
             var harmony = new Harmony("rimworld.xavior.glittertech");
             harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch]
+    public static class TraitSet_HasTrait_Patch
+    {
+        // Define the exact method signature to avoid ambiguity
+        static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(typeof(TraitSet), "HasTrait", new Type[] { typeof(TraitDef) });
+        }
+
+        static void Postfix(TraitDef tDef, ref bool __result, TraitSet __instance)
+        {
+            if (tDef == TraitDef.Named("AnnoyingVoice"))
+            {
+                Pawn pawn = Traverse.Create(__instance).Field("pawn").GetValue<Pawn>();
+                if (pawn.health.hediffSet.HasHediff(HediffDef.Named("GTEar")))
+                {
+                    __result = false; // Pretend the pawn doesn't have the trait if they have the advanced bionic ear
+                }
+            }
         }
     }
 }

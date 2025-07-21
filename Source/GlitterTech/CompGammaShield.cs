db1254032363ba89a802acac17509c5284873745
Michael Rainey <mdrain18@gmail.com>
2025-07-20 23:55:57 -0400

    Fixed Bionics for just Core no DLCs
    I've always tested with DLCs so I didnt notice that the bionics were pointing to the royalty parent def and not the core parent def.

---
+++ Source/GlitterTech/CompGammaShield.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+
+namespace GlitterTech
+{
+    public class CompProperties_GammaShield : CompProperties
+    {
+        public CompProperties_GammaShield()
+        {
+            this.compClass = typeof(CompGammaShield);
+        }
+    }
+
+    public class CompGammaShield : CompShield
+    {
+        // No override needed; logic is handled via Harmony patch
+    }
+}
---
+++ Source/GlitterTech/Patches/GammaShieldAbsorbPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace GlitterTech
+{
+    [HarmonyPatch(typeof(CompShield), nameof(CompShield.PostPreApplyDamage))]
+    public static class GammaShieldDamagePatch
+    {
+        public static void Prefix(ref DamageInfo dinfo, CompShield __instance)
+        {
+            if (__instance?.parent?.def?.defName == "Apparel_GammaShieldBelt" &&
+                dinfo.Def.defName == "BeamBypassShields" &&
+                ModsConfig.IsActive("ludeon.rimworld.odyssey"))
+            {
+                // Temporarily override the ignoreShields flag
+                AccessTools.FieldRefAccess<DamageInfo, bool>(ref dinfo, "ignoreShields") = false;
+            }
+        }
+    }
+}
